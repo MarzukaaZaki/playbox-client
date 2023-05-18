18c98b26ed82704c576d1e663d9a93249d167a57
MarzukaaZaki <zakimarzuka@gmail.com>
2023-05-18 22:18:39 +0600

    Added an error page

--- src/layouts/NotFoundLayout.jsx
+++ src/layouts/NotFoundLayout.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import not_found from '../assets/error.jpg'
 const NotFoundLayout = () => {
     return (
         <div>
-            
+            <h2> There's nothing here.</h2>
+            <img src={not_found} alt="404 Error Image" />
         </div>
     );
 };
