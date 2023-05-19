e926d65c6e37604c29b3cf4511e61808aa59d857
MarzukaaZaki <zakimarzuka@gmail.com>
2023-05-19 12:20:30 +0600

    Added Navbar with conditional user photo and links

--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,25 +1,95 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../providers/AuthProviders';
+import { Link } from 'react-router-dom';
+import pic from '../../../assets/user.jpg'
+import { Tooltip } from 'daisyui';
 
 const Header = () => {
-    const {user, logOut} = useContext(AuthContext);
-    const handleLogOut = () =>{
+    const { user, logOut } = useContext(AuthContext);
+    const handleLogOut = () => {
         logOut()
-        .then(()=>{})
-        .catch(error => console.log(error.message))
+            .then(() => { })
+            .catch(error => console.log(error.message))
     }
     return (
         <div>
-            <h2>Header goes here.</h2>
-            {user ?
-                <div>
-                    Hello, {user.displayName}
-                </div>:
-                ''
-                
-            }
-            <button className='btn btn-primary' onClick={handleLogOut}>Log Out</button>
-            
+            <div className="navbar bg-base-100">
+                <div className="navbar-start hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><Link>Home</Link></li>
+                        <li><Link>About</Link></li>
+                        <li><Link>Toys</Link></li>
+                        <li><Link>Blog</Link></li>
+
+                    </ul>
+                </div>
+                <div className="navbar-center">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><a>Home</a></li>
+                            <li><a>About</a></li>
+                            <li><a>Toys</a></li>
+                            <li><a>Blog</a></li>
+
+                            <li><a>See My Toys</a></li>
+                            <li><a>Add New Toy</a></li>
+
+
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost normal-case text-xl">PLAYBOX</a>
+                </div>
+                <div className="navbar-end">
+
+                    { user ?
+                        <div>
+                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full me-2" >
+                                    
+                                    <img src={user.photoURL} className='tooltip text-black' data-tip ={user.displayName}/>
+                                   
+                                    
+                                </div>
+                            </label>
+
+
+
+                            <div className="dropdown dropdown-end">
+                                <label tabIndex={0} className="btn btn-secondary">
+                                    Choose an Action
+                                </label>
+                                <ul tabIndex={0} className="menu menu-compact dropdown-content p-2 shadow bg-base-100 rounded-box w-52">
+                                    <li><Link>See My Toys</Link></li>
+                                    <li><Link>Add New Toy</Link></li>
+                                    <li><Link onClick={handleLogOut}>Logout</Link></li>
+                                </ul>
+                            </div>
+                        </div>
+                        :
+                        <div>
+                            <Link to='/register' className='me-8'>Register</Link>
+                            <Link to='/login' className='me-6'>Log In</Link>
+                        </div>
+
+                    }
+
+
+
+
+
+
+
+                </div>
+            </div>
+
+
+
+
+
+
         </div>
     );
 };
