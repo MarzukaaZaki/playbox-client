7af83817f8d7cddabdcd1ca68d14364a1db27ff5
MarzukaaZaki <zakimarzuka@gmail.com>
2023-05-21 23:37:59 +0600

    Added fade-up effect from aos package

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import Header from '../pages/shared/Header/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/shared/Footer/Footer';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 const Main = () => {
+    useEffect(() => {
+        AOS.init();
+      }, []);
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
+            <div data-aos="fade-up">
+                <Outlet></Outlet>
+            </div>
+            
             <Footer></Footer>
         </div>
     );
